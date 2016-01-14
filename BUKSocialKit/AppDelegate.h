cb536182108991e771304289e8a705eec9216007
LazyClutch <lr_5146@163.com>
2016-01-14 13:47:10 +0800

    init

---
+++ BUKSocialKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BUKSocialKit
+//
+//  Created by Lazy on 1/14/16.
+//  Copyright © 2016 Lazy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
