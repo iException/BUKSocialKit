59c871f4f57c0cf6ddd4b063ff84694446ffebaf
LazyClutch <lr_5146@163.com>
2016-01-15 16:31:15 +0800

    去掉用不到的错误码

--- BUKSocialKit/BUKSocialShareManager.h
+++ BUKSocialKit/BUKSocialShareManager.h
@@ -12,20 +12,14 @@
 typedef NS_ENUM(NSInteger, BUKSocialShareResultCode){
     BUKSocialShareResultCodeSuccess            = 200,        //成功
     BUKSocialShareResultCodeTokenInvalid       = 400,        //授权用户token错误
-    BUKSocialShareResultCodeBaned              = 505,        //用户被封禁
-    BUKSocialShareResultCodeFaild              = 510,        //发送失败（由于内容不符合要求或者其他原因）
-    BUKSocialShareResultCodeArgumentsError     = 522,        //参数错误,提供的参数不符合要求
-    BUKSocialShareResultCodeEmptyContent       = 5007,       //发送内容为空
-    BUKSocialShareResultCodeContentNotSupported= 5008,       //发送内容不支持
-    BUKSocialShareResultCodeShareRepeated      = 5016,       //分享内容重复
-    BUKSocialShareResultCodeGetNoUidFromOauth  = 5020,       //授权之后没有得到用户uid
-    BUKSocialShareResultCodeAccessTokenExpired = 5027,       //token过期
-    BUKSocialShareResultCodeNetworkError       = 5050,       //网络错误
-    BUKSocialShareResultCodeGetProfileFailed   = 5051,       //获取账户失败
-    BUKSocialShareResultCodeCancel             = 5052,       //用户取消授权
-    BUKSocialShareResultCodeNotLogin           = 5053,       //用户没有登录
-    BUKSocialShareResultCodeNotInstallClient   = 5054,       //用户没有安装客户端
-    BUKSocialShareResultCodeNotRegisted        = 5055,       //用户没有授权
+    BUKSocialShareResultCodeFaild              = 5000,        //发送失败（由于内容不符合要求或者其他原因）
+    BUKSocialShareResultCodeArgumentsError     = 5001,        //参数错误,提供的参数不符合要求
+    BUKSocialShareResultCodeEmptyContent       = 5002,       //发送内容为空
+    BUKSocialShareResultCodeContentNotSupported= 5003,       //发送内容不支持
+    BUKSocialShareResultCodeNetworkError       = 5004,       //网络错误
+    BUKSocialShareResultCodeCancel             = 5005,       //用户取消授权
+    BUKSocialShareResultCodeNotInstallClient   = 5006,       //用户没有安装客户端
+    BUKSocialShareResultCodeNotRegisted        = 5007,       //用户没有授权
     BUKSocialShareResultCodeNoApiAuthority     = 100031      //QQ空间应用没有在QQ互联平台上申请上传图片到相册的权限
 };
 
